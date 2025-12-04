de710284f911443f9322b27e67f045f572f7192a
Gustavo Motta <gustavohrmotta@gmail.com>
2025-12-04 13:42:22 +0000

    Finished password generator

--- src/password-generator/index.ts
+++ src/password-generator/index.ts
@@ -1,7 +1,10 @@
-const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lower = "abcdefghijklmnopqrstuvwxyz";
-const numbers = "0123456789";
-const special = "!@#$%&*+=()[]{}^~;:?/_-,.";
+const chars = {
+  upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lower: "abcdefghijklmnopqrstuvwxyz",
+  numbers: "0123456789",
+  symbols: "!@#$%&*+=()[]{}^~;:?/_-,.",
+};
+type CharKey = keyof typeof chars;
 
 const getRandom = (arr: string, len: number): string =>
   arr[Math.floor(Math.random() * len)];
@@ -26,21 +29,64 @@ const scramble = (input: string): string => {
   return chars.join("");
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  const lenghtInput = document.getElementById("pw-len");
-  const upperCheckbox = document.getElementById("upper");
-  const lowerCheckbox = document.getElementById("lower");
-  const numbersCheckbox = document.getElementById("numbers");
-  const symbolsCheckbox = document.getElementById("symbols");
+const copyToClipboard = (text: string): void => {
+  try {
+    navigator.clipboard.writeText(text);
+  } catch (err) {
+    console.error(`Error while copying text to clipboard: ${err}`);
+  }
+};
 
+document.addEventListener("DOMContentLoaded", () => {
+  const pw = document.getElementById("pw");
+  const regenBtn = document.getElementById("regen");
+  const copyBtn = document.getElementById("copy");
+  const lengthInput = document.getElementById("pw-len");
   const lengthText = document.getElementById("pw-len-text");
+  const checkboxes = document.querySelectorAll("input[type=checkbox]");
+
+  const generatePassword = (): void => {
+    let password: string = "";
+    const len: number =
+      parseInt((lengthInput as HTMLInputElement)?.value) || 12;
+
+    let variants: string[] = [];
+    checkboxes.forEach((checkbox) => {
+      if ((checkbox as HTMLInputElement).checked)
+        variants.push(chars[checkbox.id as CharKey]);
+    });
+    if (variants.length === 0) return;
+
+    const minEach = Math.floor(len / variants.length);
+    variants.forEach((variant) => {
+      password += getChars(variant, minEach);
+    });
+
+    const allChars = variants.join("");
+
+    while (password.length < len) {
+      password += getRandom(allChars, allChars.length);
+    }
+
+    if (pw) pw.textContent = scramble(password);
+  };
 
   if (lengthText) {
-    lenghtInput?.addEventListener("input", (e) => {
+    lengthInput?.addEventListener("input", (e) => {
       const len = (e.target as HTMLInputElement)?.value || "8";
       lengthText.textContent = len;
+      generatePassword();
     });
   } else {
     throw new Error("Length text element not found.");
   }
+
+  regenBtn?.addEventListener("click", generatePassword);
+  copyBtn?.addEventListener("click", () => {
+    if (pw) copyToClipboard(pw.textContent);
+  });
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", generatePassword);
+  });
+  generatePassword();
 });
